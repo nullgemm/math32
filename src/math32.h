d91c6c9a33f94357413983a31472731dc0130020
nullgemm <nullgemm@mailbox.org>
2019-05-12 00:36:18 +0200

    add comments in header

--- src/math32.h
+++ src/math32.h
@@ -3,11 +3,18 @@
 
 #include <stdint.h>
 
+// does not support 0 (brute-tested)
 uint8_t zeros(uint32_t x);
+// does not support 0xFFFFFFFF (brute-tested)
 uint8_t ilog10(uint32_t x);
+// does not support 0xFFFFFFFF (brute-tested)
 uint8_t ilog2(uint32_t x);
+// works as intended (brute-tested)
 uint32_t isqrt(uint32_t x);
+// works as intended (brute-tested)
 int32_t icbrt(uint32_t x);
+// seems to work as intended
+// but check the number limits!
 int32_t ipow(int32_t base, uint8_t exp);
 
 #endif
